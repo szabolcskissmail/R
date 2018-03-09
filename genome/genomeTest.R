cf127e5412053a772bb8379ff326998414da58b5
Szabolcs Kiss <32358310+szabolcskissmail@users.noreply.github.com>
2018-03-09 21:27:37 +0100

    to file

--- genome/genomeTest.R
+++ genome/genomeTest.R
@@ -4,6 +4,25 @@ randomDna <- function(length){
   return (sample(genomeTypes, length, replace = TRUE))
 }
 
-print(genomeTypes)
+dna2file <- function(filename, dna) {
+  if (file.exists(filename)) file.remove(filename)
+  pos <- 0
+  posEOL <- 0
+  for (i in dna) {
+    if(posEOL == 6){
+      cat('\n', file=filename, append=TRUE)
+      posEOL <- 0
+    } else {
+      cat(i, file=filename, append=TRUE)
+      if(pos == 9){
+        cat(' ', file=filename, append=TRUE)
+        pos <- 0
+        posEOL <- posEOL + 1
+      } else {
+        pos <- pos + 1
+      }
+    }
+  }
+}
 
-print (randomDna(50))
+print(dna2file("genome.txt", randomDna(230)))
