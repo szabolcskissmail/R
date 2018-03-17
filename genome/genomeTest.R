d205a36ce6a0d625d77c66272041eb2632913d17
Szabolcs Kiss <32358310+szabolcskissmail@users.noreply.github.com>
2018-03-17 18:36:58 +0100

    write to file

--- genome/genomeTest.R
+++ genome/genomeTest.R
@@ -1,16 +1,26 @@
+library(stringr)
+
 genomeTypes <- c('T', 'A', 'C', 'G')
 
 randomDna <- function(length){
   return (sample(genomeTypes, length, replace = TRUE))
 }
 
+formatLineCounter <- function (lineNumber){
+  return (paste(str_pad(lineNumber, 6, pad = "0"), " "))
+}
+
 dna2file <- function(filename, dna) {
   if (file.exists(filename)) file.remove(filename)
   pos <- 0
   posEOL <- 0
+  lineNumber <- 1
+  cat(formatLineCounter(lineNumber), file=filename, append=TRUE)
   for (i in dna) {
     if(posEOL == 6){
       cat('\n', file=filename, append=TRUE)
+      lineNumber <- lineNumber + 60
+      cat(formatLineCounter(lineNumber), file=filename, append=TRUE)
       posEOL <- 0
     } else {
       cat(i, file=filename, append=TRUE)
@@ -25,4 +35,4 @@ dna2file <- function(filename, dna) {
   }
 }
 
-print(dna2file("genome.txt", randomDna(230)))
+dna2file("genome.txt", randomDna(650))
